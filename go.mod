cff7f274b6fb3f8b73724800c9390db94cdd676f
Tom Goren <tom@tomgoren.com>
2024-03-09 14:49:47 -0500

    WIP

--- go.mod
+++ go.mod
@@ -3,15 +3,27 @@ module github.com/tomgoren/linear-cli
 go 1.22.0
 
 require (
+	github.com/charmbracelet/bubbletea v0.25.0
 	github.com/hasura/go-graphql-client v0.12.0
-	github.com/jedib0t/go-pretty/v6 v6.5.4
 	github.com/pelletier/go-toml v1.9.5
 )
 
 require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/term v0.16.0 // indirect
+	golang.org/x/text v0.3.8 // indirect
 	nhooyr.io/websocket v1.8.10 // indirect
 )
--- main.go
+++ main.go
@@ -7,9 +7,8 @@ import (
 	"os"
 	"strings"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/hasura/go-graphql-client"
-	"github.com/jedib0t/go-pretty/v6/table"
-	"github.com/jedib0t/go-pretty/v6/text"
 	toml "github.com/pelletier/go-toml"
 )
 
@@ -57,9 +56,23 @@ func getMyId(client *graphql.Client) string {
 }
 
 type issues []struct {
-	Id    string
-	Title string
-	Url   string
+	Id         string
+	Identifier string
+	Title      string
+	Url        string
+}
+
+func getLinearClient() *graphql.Client {
+	config := getConfig()
+	apiToken := config.Get("auth.api_token").(string)
+	httpClient := &http.Client{Transport: &authenticatedTransport{apiToken: apiToken}}
+	return graphql.NewClient(linearUrl, httpClient).WithDebug(true)
+}
+
+func getMyIssuesCmd() tea.Msg {
+	linearClient := getLinearClient()
+	myId := getMyId(linearClient)
+	return getIssuesForUserId(linearClient, myId)
 }
 
 func getIssuesForUserId(client *graphql.Client, id string) issues {
@@ -84,28 +97,125 @@ func getIssuesForUserId(client *graphql.Client, id string) issues {
 	return query.User.AssignedIssues.Nodes
 }
 
-func getTable(issues issues) string {
-	t := table.NewWriter()
-	// t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"Title", "URL"})
-	for _, issue := range issues {
-		t.AppendRows([]table.Row{{text.WrapSoft(issue.Title, 55), getShortUrl(issue.Url)}})
-		t.AppendSeparator()
-	}
-	return t.Render()
+// func getTable(issues issues) string {
+// 	t := table.NewWriter()
+// 	// t.SetOutputMirror(os.Stdout)
+// 	t.AppendHeader(table.Row{"Title", "Identifier"})
+// 	for _, issue := range issues {
+// 		t.AppendRows([]table.Row{{text.WrapSoft(issue.Title, 55), issue.Identifier}})
+// 		t.AppendSeparator()
+// 	}
+// 	return t.Render()
+// }
+
+//	func getShortUrl(url string) string {
+//		return url[0:9]
+//	}
+type model struct {
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
 }
 
-func getShortUrl(url string) string {
-	return url[0:9]
+func initialModel() model {
+	return model{}
+	// 	choices: []string,
+	//
+	// 	// A map which indicates which choices are selected. We're using
+	// 	// the  map like a mathematical set. The keys refer to the indexes
+	// 	// of the `choices` slice, above.
+	// 	selected: make(map[int]struct{}),
+	// }
+}
+
+func (m model) Init() tea.Cmd {
+	// Just return `nil`, which means "no I/O right now, please."
+	return getMyIssuesCmd
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	// Is it a key press?
+	case issues:
+		for _, issue := range msg {
+			m.choices = append(m.choices, fmt.Sprintf("%s\t%s", issue.Title, issue.Identifier))
+		}
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
+}
+
+func (m model) View() string {
+	// The header
+	s := "Select an issue\n"
+	s += "Title\tIdentifier\n"
+
+	// Iterate over our choices
+	for i, choice := range m.choices {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		// Is this choice selected?
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "x" // selected!
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	// Send the UI for rendering
+	return s
 }
 
 func main() {
-	config := getConfig()
-	apiToken := config.Get("auth.api_token").(string)
-	httpClient := &http.Client{Transport: &authenticatedTransport{apiToken: apiToken}}
-
-	client := graphql.NewClient(linearUrl, httpClient).WithDebug(true)
-	myId := getMyId(client)
-	myIssues := getIssuesForUserId(client, myId)
-	fmt.Println(getTable(myIssues))
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+	// myId := getMyId(client)
+	// myIssues := getIssuesForUserId(client, myId)
+	// fmt.Println(myIssues)
 }
